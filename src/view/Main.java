0332b9ae163add51e0fa16c26e4ef9e3aace4b02
Everton Santos <everton_santos_01@hotmail.com>
2022-06-22 21:11:14 -0300

    Primeiro coommit
    Primeiro coommit

--- src/view/Main.java
+++ src/view/Main.java
@@ -8,7 +8,7 @@ import controller.LogarGoogleAdapter;
 
 public class Main {
 	public static void main(String[] args) {
-	    Logar logar = LogarBuilder.builder().Login("Gabriel Guerra", "fatec123").Confirmacao(4321).get();
+	    Logar logar = LogarBuilder.builder().Login("Everton Santos", "fatec123").Confirmacao(4321).get();
 	    LogarGoogle loginGoo = new LogarGoogle();
 	    ILogar servicoLogar = new LogarGoogleAdapter(loginGoo);
 	    servicoLogar.logar(logar);
